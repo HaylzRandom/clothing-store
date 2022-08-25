17b2a85a89af744a8411ec58e6b18ace33bb637d
hayley.mcc@gmail.com <hayley.mcc@gmail.com>
2022-08-25 12:05:05 +0100

    Updated cart and user context to use reducers

--- src/contexts/user.js
+++ src/contexts/user.js
@@ -1,4 +1,6 @@
-import { createContext, useState, useEffect } from 'react';
+import { createContext, useEffect, useReducer } from 'react';
+
+import { createAction } from '../utils/reducer/reducer';
 
 // Firebase Utils
 import {
@@ -12,8 +14,36 @@ export const UserContext = createContext({
 	setCurrentUser: () => null,
 });
 
+export const USER_ACTIONS_TYPES = {
+	SET_CURRENT_USER: 'SET_CURRENT_USER',
+};
+
+const userReducer = (state, action) => {
+	const { type, payload } = action;
+
+	switch (type) {
+		case USER_ACTIONS_TYPES.SET_CURRENT_USER:
+			return {
+				...state,
+				currentUser: payload,
+			};
+		default:
+			throw new Error(`Unhandled type ${type} in userReducer`);
+	}
+};
+
+const INITIAL_STATE = {
+	currentUser: null,
+};
+
 export const UserProvider = ({ children }) => {
-	const [currentUser, setCurrentUser] = useState(null);
+	const [{ currentUser }, dispatch] = useReducer(userReducer, INITIAL_STATE);
+	console.log(currentUser);
+
+	const setCurrentUser = (user) => {
+		dispatch(createAction(USER_ACTIONS_TYPES.SET_CURRENT_USER, user));
+	};
+
 	const value = { currentUser, setCurrentUser };
 
 	useEffect(() => {
